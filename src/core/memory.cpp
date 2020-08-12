432bc1d80fde8d2dedb6cd5e6e9082e06253497d
Yunhao Mao <yunhaom94@outlook.com>
2020-08-11 21:50:33 -0400

    update memory

--- src/core/memory.cpp
+++ src/core/memory.cpp
@@ -1,63 +1,24 @@
 #include "core/memory.h"
 
 
-void* AllocAligned(size_t size)
-{
-#if defined(PBRT_IS_WINDOWS)
-	return _aligned_malloc(size, PBRT_L1_CACHE_LINE_SIZE);
-#elif defined (PBRT_IS_OPENBSD) || defined(PBRT_IS_OSX)
-	void* ptr;
-	if (posix_memalign(&ptr, PBRT_L1_CACHE_LINE_SIZE, size) != 0)
-		ptr = nullptr;
-	return ptr;
+
+void* AllocAligned(size_t size) {
+#if defined(PBRT_HAVE__ALIGNED_MALLOC)
+    return _aligned_malloc(size, PBRT_L1_CACHE_LINE_SIZE);
+#elif defined(PBRT_HAVE_POSIX_MEMALIGN)
+    void* ptr;
+    if (posix_memalign(&ptr, PBRT_L1_CACHE_LINE_SIZE, size) != 0) ptr = nullptr;
+    return ptr;
 #else
-	return memalign(PBRT_L1_CACHE_LINE_SIZE, size);
+    return memalign(PBRT_L1_CACHE_LINE_SIZE, size);
 #endif
 }
 
-
-
-void FreeAligned(void* ptr)
-{
-	if (!ptr)
-		return;
-
-	_aligned_free(ptr);
+void FreeAligned(void* ptr) {
+    if (!ptr) return;
+#if defined(PBRT_HAVE__ALIGNED_MALLOC)
+    _aligned_free(ptr);
+#else
+    free(ptr);
+#endif
 }
-
-void* MemoryArena::Alloc(size_t nBytes)
-{
-	nBytes = ((nBytes + 15) & (~15));
-
-	if (currentBlockPos + nBytes > currentAllocSize)
-	{
-		if (currentBlock) 
-		{
-			usedBlocks.push_back(std::make_pair(currentAllocSize, currentBlock));
-			currentBlock = nullptr;
-		}
-		for (auto iter = availableBlocks.begin(); iter != availableBlocks.end();
-			++iter) 
-		{
-			if (iter->first >= nBytes) 
-			{
-				currentAllocSize = iter->first;
-				currentBlock = iter->second;
-				availableBlocks.erase(iter);
-				break;
-			}
-
-			if (!currentBlock) 
-			{
-				currentAllocSize = std::max(nBytes, blockSize);
-				currentBlock = AllocAligned<uint8_t>(currentAllocSize);
-			}
-			currentBlockPos = 0;
-
-		}
-	}
-	void* ret = currentBlock + currentBlockPos;
-	currentBlockPos += nBytes;
-	return ret;
-}
-
